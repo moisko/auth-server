075988eb190780d648c59fab8e9f9dc5444f2299
Boris Borisov <boris.r.borisov@gmail.com>
2018-02-23 19:49:29 +0200

    move to app folder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const router = require('./router');
+const router = require('./app/router');
 const cors = require('cors');
 
 const app = express();
@@ -13,17 +13,10 @@ const app = express();
 mongoose.connect('mongodb://heroku_gg35vrsj:g2rv6mbs6g3k2ml3qni20r8fbe@ds147518.mlab.com:47518/heroku_gg35vrsj');
 
 // App Setup
-app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('http://' + req.hostname + req.url);
-    } else {
-        next();
-    }
-});
-
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({type: '*/*'}));
+
 router(app);
 
 // Server Setup
